8d2184c1cf4d4d362331545833ff90267eab0f1d
Duo 'Erica' Xu <xdd.erica@gmail.com>
2022-01-20 15:22:51 -0500

    Update atacSeq_cluster.R

--- atacSeq_cluster.R
+++ atacSeq_cluster.R
@@ -5,23 +5,6 @@ library(pheatmap)
 library(CancerSubtypes)
 library(edgeR)
 
-########################################################################################################################
-# getting the top variable peaks using counts with replica
-countData_wRep <- read.csv('featureCountMatrix_idr_all.csv', header = T, sep = ',', row.names = 1)
-metaData_wRep <- read.csv('orgn_sampleList_wRep.txt', header = F, sep = "\t", row.names = 1)
-consensusToCount <- readRDS('consensusToCount_idr.RDS')
-
-colnames(countData_wRep)[c(1,2)]
-colnames(countData_wRep)[1] <- '22Rv1_rep1'
-colnames(countData_wRep)[2] <- '22Rv1_rep2'
-
-dds_wRep <- DESeqDataSetFromMatrix(countData_wRep, metaData_wRep, ~ V2, rowRanges = consensusToCount, tidy = F)
-dds_wRep <- DESeq(dds_wRep)
-
-# log
-normCounts_wRep <- counts(dds_wRep, normalized =TRUE)
-rlog_wRep <- log2(normCounts_wRep+1)
-
 # top % variable peaks function
 top_var_gene_var <- function(input_matrix, threshold) {
   select <- head(order(rowVars(input_matrix),decreasing=TRUE),threshold)
@@ -29,21 +12,16 @@ top_var_gene_var <- function(input_matrix, threshold) {
   return(topVarGenes)
 }
 
-peak_topvar <- top_var_gene_var(rlog_wRep, as.integer(dim(rlog_wRep)[1]/100))
-
-sample_cor <- cor(peak_topvar, method = "pearson")
-annotation = data.frame(factor(metaData_wRep$V6))
-rownames(annotation) = rownames(metaData_wRep)
-pheatmap(sample_cor, cluster_rows=TRUE, show_rownames=TRUE, annotation = annotation)
-
 
 ########################################################################################################################
 # do clustering using counts without replica
 countData <- read.csv('featureCountMatrix_idr_ave.csv', header = T, sep = ',', row.names = 1)
-metaData <- read.csv('../EX_0201_prostateSubtypePredict/orgn_sampleList_wPDX.txt', header = F, sep = "\t", row.names = 1)
+metaData <- read.csv('orgn_sampleList_wPDX.txt', header = F, sep = "\t", row.names = 1)
+consensusToCount <- readRDS('consensusToCount_idr.RDS')
 
 colnames(countData)[1]
 colnames(countData)[1] <- '22Rv1'
+
 
 dds <- DESeqDataSetFromMatrix(countData=countData, colData = metaData, rowRanges = consensusToCount,
                               tidy = F, design=~1)
@@ -56,21 +34,16 @@ rlog <- log2(normCounts+1)
 
 rlog <- rlog[,rownames(metaData)]
 
+peak_topvar <- top_var_gene_var(rlog, as.integer(dim(rlog)[1]/100))
+
 rlog_ave_topVar <- rlog[row.names(peak_topvar),]
 rlog_ave_topVar <- sweep(rlog_ave_topVar,1, apply(rlog_ave_topVar,1,median,na.rm=T))
-dim(rlog_ave_topVar)
 
-# Use top 1% variable peaks from with repeats (log2(norm+1)), and then center by median for clustering
-# euclidean distance, hc, pItem anything > 0.98
-results = ConsensusClusterPlus(as.matrix(rlog_ave_topVar),maxK=6,reps=1000,pItem=0.98,
-                               pFeature=1,innerLinkage="ward.D2", finalLinkage="ward.D2",
-                               title="consensusCluster_ave", distance='euclidean', writeTable=T,
-                               clusterAlg="hc",plot="pdf",seed=42)
-
-
-calcRes <- calcICL(results, title = "consensusCluster_ave", plot = 'pdf')
-write.csv(calcRes$clusterConsensus, 'consensusCluster_ave/clusterConsensus.csv', quote=F, row.names = F)
-write.csv(calcRes$itemConsensus, 'consensusCluster_ave/itemConsensus.csv', quote=F, row.names = F)
+# Use top 1% variable peaks, (log2(norm+1)), and then center by median for clustering
+results = ConsensusClusterPlus(as.matrix(rlog_ave_topVar),maxK=6,reps=1000,pItem=0.9,
+                               pFeature=1,innerLinkage="ward.D", finalLinkage="ward.D2",
+                               title="consensusCluster_ave", distance='pearson', writeTable=T,
+                               clusterAlg="hc",plot="pdf",seed=23445.61)
 
 ccLabel <- data.frame(factor(results[[4]]$consensusClass))
 colnames(ccLabel) <- 'ccPredict'
@@ -82,4 +55,12 @@ colnames(ccMatrix) <- names(results[[4]]$consensusClass)
 pheatmap(ccMatrix, cluster_rows=results[[4]]$consensusTree, cluster_cols=results[[4]]$consensusTree,
          show_rownames=TRUE, annotation_row = ccLabel, annotation_col = annotation, fontsize=6)
 dev.off()
+        
+         
+calcRes <- calcICL(results, title = "consensusCluster_ave", plot = 'pdf')
+write.csv(calcRes$clusterConsensus, 'consensusCluster_ave/clusterConsensus.csv', quote=F, row.names = F)
+write.csv(calcRes$itemConsensus, 'consensusCluster_ave/itemConsensus.csv', quote=F, row.names = F)
+
+
+
 
